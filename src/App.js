0bebe9738ab7b381a60a6d11668d0d8df8333d04
baghelharsh200 <159938238+baghelharsh200@users.noreply.github.com>
2024-12-29 15:47:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Welcome to Node.js Frontend on Tomcat 9!</h1>
+      <p>This is a sample frontend deployed on a Tomcat server.</p>
+    </div>
+  );
+}
+
+export default App;
